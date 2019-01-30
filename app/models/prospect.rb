bf7084edf5c66ae60e4d4c1fbdf66c28abae482e
Lenox Ramsey <lenoxramseyjr@outlook.com>
2019-01-30 07:09:39 +0000

    Added 1:m validation to prospect model

--- app/models/prospect.rb
+++ app/models/prospect.rb
@@ -1,6 +1,6 @@
 class Prospect < ApplicationRecord
     validates :committeename, presence: true
     validates :contactemail, presence: true
-    
+    validates :user_id, presence: true
     belongs_to :user
 end
